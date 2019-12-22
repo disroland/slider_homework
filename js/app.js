392f5081bdf2bd4a638348e11f4ae5a8bf442a02
disroland <disroland@gmail.com>
2019-12-22 09:49:58 +0200

    test

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 
 $('.multiple-items').slick({
   infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToShow: 2,
+  slidesToScroll: 1,
   dots:true
 });
 
